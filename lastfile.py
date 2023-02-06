6eb2882f018750727d00dbb4dfe1794838ab0908
hex <admin@MacBook-Air-Admin.local>
2023-02-06 13:51:29 +0300

    modified:   lastfile.py

--- lastfile.py
+++ lastfile.py
@@ -3,23 +3,22 @@ import requests
 
 URL = 'http://127.0.0.1'
 
-def read_file(last_file):
+def read_file():
+    file = ''
+    with open('/home/klipper/printer_button/lastfile.txt', 'r') as out:
+        file = out.read()
+        out.close()
+        return file
+
+while True:
     try:
-        file = ''
-        with open('/home/klipper/printer_button/lastfile.txt', 'r') as out:
-            file = out.read()
-            out.close()
-            return last_file == file
+        moonraker_file = requests.get(URL + '/printer/objects/query?print_stats').json()['result']['status']['print_stats']['filename']
+        lasfile_txt = read_file()
+        if not moonraker_file == '':
+            if not moonraker_file == lasfile_txt:
+                with open('/home/klipper/printer_button/lastfile.txt', 'w') as out:
+                    out.write(moonraker_file)
+                    out.close()
+        time.sleep(5)
     except:
-        read_file(last_file)
-
-last_file = requests.get(URL + '/printer/objects/query?print_stats').json()['result']['status']['print_stats']['filename']
-
-while read_file(last_file) or last_file == '':
-    last_file = requests.get(URL + '/printer/objects/query?print_stats').json()['result']['status']['print_stats']['filename']
-
-with open('/home/klipper/printer_button/lastfile.txt', 'w') as out:
-    out.write(last_file)
-    out.close()
-
-time.sleep(20)
+        time.sleep(5)
